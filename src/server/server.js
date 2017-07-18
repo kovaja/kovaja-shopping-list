ec463efe1b9fce3e70e328423d9aa39b41fd24fc
kovaja <kovaja@users.noreply.github.com>
2017-07-18 08:10:44 +0200

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
-    res.setHeader(‘Access-Control-Allow-Origin’, 'https://kovaja-shopping-list.herokuapp.com/*')
+    //res.setHeader(‘Access-Control-Allow-Origin’, 'https://kovaja-shopping-list.herokuapp.com/*')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
     res.setHeader('Access-Control-Allow-Headers',
             'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
